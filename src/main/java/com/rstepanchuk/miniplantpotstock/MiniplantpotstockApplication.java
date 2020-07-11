4f4241f1f6156535b4dec9629722fbdc72ccc2fb
Rostyslav Stepanchuk <rostyslav.stepanchuk@gmail.com>
2020-07-11 15:55:05 +0300

    first endpoint controller

---
+++ src/main/java/com/rstepanchuk/miniplantpotstock/MiniplantpotstockApplication.java
@@ -0,0 +1,13 @@
+package com.rstepanchuk.miniplantpotstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniplantpotstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniplantpotstockApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/rstepanchuk/miniplantpotstock/controller/RootController.java
@@ -0,0 +1,14 @@
+package com.rstepanchuk.miniplantpotstock.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+  @GetMapping
+  public ResponseEntity<String> redirectToMobile(){
+    return ResponseEntity.ok("Some day this will be a cool application");
+  }
+}
