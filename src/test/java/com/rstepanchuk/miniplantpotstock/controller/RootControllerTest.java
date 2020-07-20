59a24bc10ae48085793fbc011e4cb98f767bbdf4
RostyslavStepanchuk <48453944+RostyslavStepanchuk@users.noreply.github.com>
2020-07-20 23:31:07 +0300

    coverAlls (#4)
    * coverAlls
    
    Co-authored-by: Rostyslav Stepanchuk <rostyslav.stepanchuk@gmail.com>

--- src/test/java/com/rstepanchuk/miniplantpotstock/controller/RootControllerTest.java
+++ src/test/java/com/rstepanchuk/miniplantpotstock/controller/RootControllerTest.java
@@ -1,7 +1,28 @@
 package com.rstepanchuk.miniplantpotstock.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class RootControllerTest {
 
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  public void accessTokenWithValidCredentialsShouldReturnToken() throws Exception {
+
+    RequestBuilder requestBuilder = get("/");
+
+    mockMvc.perform(requestBuilder)
+        .andExpect(status().isOk());
+ }
 }
