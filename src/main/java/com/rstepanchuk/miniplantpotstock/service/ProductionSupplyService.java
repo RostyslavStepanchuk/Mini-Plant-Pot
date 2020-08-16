f43e797555fb24e224c1ee500ba686e347f19806
Rostyslav Stepanchuk <rostyslav.stepanchuk@gmail.com>
2020-08-16 19:38:35 +0300

    Optimized imports

--- src/main/java/com/rstepanchuk/miniplantpotstock/service/ProductionSupplyService.java
+++ src/main/java/com/rstepanchuk/miniplantpotstock/service/ProductionSupplyService.java
@@ -5,7 +5,6 @@ import com.rstepanchuk.miniplantpotstock.repository.ProductionSupplyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 
 @Service
